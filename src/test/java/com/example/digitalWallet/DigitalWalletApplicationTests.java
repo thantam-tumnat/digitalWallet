7e29993b96f9e3b685b65557c04cb54d3f99129e
unknown <ca.tumnat@gmail.com>
2026-07-23 05:50:45 +0700

    first commit

---
+++ src/test/java/com/example/digitalWallet/DigitalWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.digitalWallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
